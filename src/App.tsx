018b23c9576763a9eab31d56123a1c685cffcb94
thayron <thayronf3@gmail.com>
2021-09-16 07:04:10 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { GlobalStyle } from './styles/globol';
 
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <GlobalStyle />
     </div>
   );
 }
