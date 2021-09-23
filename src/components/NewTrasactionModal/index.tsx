6fe828c489fff91f7d291fa5e95b4a5777c07621
thayron <thayronf3@gmail.com>
2021-09-23 07:29:04 -0300

    Foi realizado a estilizaçao do modal

--- src/components/NewTrasactionModal/index.tsx
+++ src/components/NewTrasactionModal/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import Modal from 'react-modal'
+import Modal from 'react-modal';
+import { Container } from './styles';
+import close from '../../assets/close.svg';
+
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose(): void;
 }
-// import { Container } from './styles';
 
-export const NewTrasactionModal: React.FC <NewTransactionModalProps> = ({
+export const NewTrasactionModal: React.FC<NewTransactionModalProps> = ({
   isOpen,
   onRequestClose
 }) => (
   <Modal
     isOpen={isOpen}
-    onRequestClose={onRequestClose}>
-    <h2>Cadastrar transação</h2>
+    onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
+  >
+    <button
+      type="button"
+      onClick={onRequestClose}
+      className="react-modal-close">
+      <img src={close} alt="Fechar modal" />
+    </button>
+    <Container>
+
+      <h2>Cadastrar transação</h2>
+      <input type="" placeholder="Título" />
+      <input type="text" placeholder="Valor" />
+      <input type="text" placeholder="Categoria" />
+      <button type="submit" > Cadastar</button>
+    </Container>
+
   </Modal>
 )
